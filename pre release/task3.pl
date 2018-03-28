659e6116aeeb75531801d1d799de0cf758ef45fe
zhangtrex <zcy2000615@126.com>
2018-03-28 11:49:59 +0800

    task 3

--- pre release/task3.pl
+++ pre release/task3.pl
@@ -11,7 +11,7 @@ animal(bird).
 skill(fly).
 skill(invisibility).
 
-
+/* 3.1 */
 character(habib).
 animal(fish).
 skill(time_travel).
@@ -20,12 +20,13 @@ pet(habib,fish).
 character_type(habib,explorer).
 has_skill(habib,timetravel).
 
+/* 3.2 */
+pet(jim,bird).
+onlyapet(X,Y) :-
+  animal(Y),
+  character(X).
 
-onlyonepet(X,Y) :-
-  character(X),
-  animal(Y).
-
-
+/* 3.3 */
 character(rex).
 animal(dog).
 skill(singularity).
@@ -33,4 +34,18 @@ skill(singularity).
 pet(rex,dog).
 character_type(rex,scientist).
 has_skill(rex,singularity).
+
+/* 3.4 */
+/* true
+ princess
+ jim
+ invisibility
+ jim
+*/
+
+/* 3.5 */
+/* pet(jim,Y).
+ has_skill(X,fly).
+ skill(X).
+ character_type(X,princess),pet(X,Y). */
 
